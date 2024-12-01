a6658225e4bb609104e9c1d8bba32f7acc873de6
Giacomo Pilotti <jack.pilo04@gmail.com>
2024-12-01 19:07:55 +0100

    Make it b3u4t1ful3r

--- day_01/common.py
+++ day_01/common.py
@@ -3,5 +3,5 @@ from typing import Tuple, List
 
 def parse_input() -> Tuple[List[int], List[int]]:
     with open("input", "r") as f:
-        lists = [[int(k) for k in i.split("   ")] for i in f.readlines()]
-    return map(list, zip(*lists))
+        lists = [list(map(int, line.split())) for line in f]
+    return tuple(map(list, zip(*lists)))
