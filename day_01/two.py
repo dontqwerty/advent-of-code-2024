4cfa29fcc2c0c33e5ec08660fd54e142560f9449
Giacomo Pilotti <jack.pilo04@gmail.com>
2024-12-01 19:08:15 +0100

    Make it f4st3r

--- day_01/two.py
+++ day_01/two.py
@@ -3,9 +3,5 @@ from common import parse_input
 left, right = parse_input()
 sum = 0
 for i in left:
-    i_in_right = 0
-    for k in right:
-        if i == k:
-            i_in_right += 1
-    sum += i * i_in_right
+    sum += i * right.count(i)
 print(sum)
