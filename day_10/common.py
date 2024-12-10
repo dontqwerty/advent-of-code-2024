0820aeb4d60ea9150536ea17c404bdc58057c36a
Giacomo Pilotti <jack.pilo04@gmail.com>
2024-12-10 07:56:51 +0100

    Day 10

---
+++ day_10/common.py
@@ -0,0 +1,2 @@
+def parse_input():
+    return [list(map(int, line.strip())) for line in open("input").readlines()]
---
+++ day_10/one.py
@@ -0,0 +1,30 @@
+from common import parse_input
+
+lines = parse_input()
+peaks = set()
+
+
+def hike(ii, ij):
+    if lines[ii][ij] == 9 and (ii, ij) not in peaks:
+        peaks.add((ii, ij))
+        return 1
+    score = 0
+    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        i, j = ii + di, ij + dj
+        if (
+            not (0 <= i < len(lines) and 0 <= j < len(lines[0]))
+            or lines[i][j] - lines[ii][ij] != 1
+        ):
+            continue
+        score += hike(i, j)
+    return score
+
+
+score = 0
+for i in range(len(lines)):
+    for j in range(len(lines[0])):
+        if lines[i][j] != 0:
+            continue
+        score += hike(i, j)
+        peaks = set()
+print(score)
